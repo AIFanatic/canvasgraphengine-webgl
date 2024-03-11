588b5795180575dd573a25418f719a8ef6573ff0
AIFanatic <aifanatic@github.com>
2024-03-11 22:44:59 -0100

    removed unused code

--- src/Color.ts
+++ src/Color.ts
@@ -2,15 +2,7 @@ export class Color {
     public static RGB(r: number, g: number, b: number): number[] {
         return [r / 255, g / 255, b / 255];
     }
-
-    // public static HEX(r: number, g: number, b: number): number {
-    //     const rp = Math.max(0, Math.min(255, r));
-    //     const gp = Math.max(0, Math.min(255, g));
-    //     const bp = Math.max(0, Math.min(255, b));
-
-    //     return (rp << 16) | (gp << 8) | bp;
-    // }
-
+    
     public static HEX(r: number, g: number, b: number): number {
         return r + g * 256.0 + b * 256.0 * 256.0;
     }
